81e3acf0bbb0e7618cea2055d0f6afa913066b78
jm1021 <jmort1021@gmail.com>
2025-06-09 08:31:22 -0700

    flask to flaskstu

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -7,7 +7,7 @@ export var pythonURI;
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     pythonURI = "http://localhost:8587";  // Same URI for localhost or 127.0.0.1
 } else {
-    pythonURI = "https://flask.opencodingsociety.com";
+    pythonURI = "https://flaskstu.opencodingsociety.com";
 
 }
 
